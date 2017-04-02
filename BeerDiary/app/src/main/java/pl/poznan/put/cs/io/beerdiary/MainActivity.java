5c204e3afed88cc4b91e5b27ca0069e179967a7f
Michał Wójcik <michal.p.wojcik@student.put.poznan.pl>
2017-04-02 23:07:51 +0200

    Start Page GUI added

--- BeerDiary/app/src/main/java/pl/poznan/put/cs/io/beerdiary/MainActivity.java
+++ BeerDiary/app/src/main/java/pl/poznan/put/cs/io/beerdiary/MainActivity.java
@@ -2,6 +2,7 @@ package pl.poznan.put.cs.io.beerdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void beerButtonOnClick(View v) {
+        findViewById(R.id.PubButton).setVisibility(View.INVISIBLE);
+    }
+
+    public void addButtonOnClick(View v) {
+        findViewById(R.id.PubButton).setVisibility(View.VISIBLE);
     }
 }
