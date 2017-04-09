f9dd0a1fce6cab1033a93a58155bf0ba9fb845f2
Jan Bajer <jan.bajer@gmail.com>
2017-04-09 10:28:40 +0200

    Added Rating & Pub classes.

---
+++ BeerDiary/app/src/main/java/pl/poznan/put/cs/io/beerdiary/Rating.java
@@ -0,0 +1,12 @@
+package pl.poznan.put.cs.io.beerdiary;
+
+/**
+ * Enum przygotowany na podstawie UML z poprzedniego semestru.
+ * Służy do przechowywania ogólnej oceny obiektów pozostałych klas, jak np. piw, pubów.
+ * Ma za zadanie ograniczyć zakres przyjmowanych ocen tylko do wartości całkowitoliczbowych 1-5 (bez 0!)
+ * Szkoda tylko, że przy tworzeniu UML nie pomyśleliśmy, że pojedyncze cyfry nie są poprawnymi identyfikatorami.
+ */
+
+public enum Rating {
+    _1, _2, _3, _4, _5;
+}
